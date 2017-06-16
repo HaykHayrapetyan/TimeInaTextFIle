f6791910ba4376295f3c37b0b73f6bc8ccaea922
Internship 5 <haykhairapetyan@gmail.com>
2017-06-16 16:59:26 +0400

    'transformovAshxatox'

---
+++ homework.js
@@ -0,0 +1,43 @@
+var moment = require('moment');
+var {Readable, Writable, Transform} = require('stream');
+var fs = require('fs');
+
+console.log(moment().format());
+
+ class FSTransform extends Transform{
+     _transform(buffer, encoding, done){
+        this.push(moment().format().toString() + "\n");
+        done();
+     }
+ }
+
+class FSReadable extends Readable{
+    
+    _read(size){
+        const time = new Date();
+        setTimeout(()=>{
+            this.push(time.toString());
+        }, 1000);  
+    }
+}
+
+class FSWritable extends Writable{
+    constructor(Option){
+        super(Option)
+        this.file = fs.openSync(__dirname + '/time.txt', "w+");
+    }
+
+    _write(chunk, encoding, done){
+        fs.write(this.file, chunk.toString(), ()=>{
+            done();
+        });  
+    }
+}
+
+
+var fsreadable = new FSReadable();
+var fswritable = new FSWritable();
+var fstrandform = new FSTransform();
+
+fsreadable.pipe(fstrandform).pipe(fswritable);
+
